1c6c81fbc48c2f9fc6d4a1cc22452ff6ee9eeb29
Thomas Schuetz <thomas.schuetz@eon.com>
2020-10-15 12:39:14 +0200

    Fixed CORS issue

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/add', methods = ['POST'])
 def add():
